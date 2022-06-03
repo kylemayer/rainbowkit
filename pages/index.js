c180a05aaf3c2dd65b5405e9408d092f02e6a7cf
Kyle Mayer <x@kylemayer.dev>
2022-06-03 12:40:05 -0700

    connect button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
@@ -13,12 +14,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          RainbowKit 🌈 demo
         </h1>
 
+
+
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <ConnectButton />
         </p>
 
         <div className={styles.grid}>
